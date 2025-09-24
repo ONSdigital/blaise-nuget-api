cd6df012471f24739d65e08bd4df9fce018e6d99
Richmond Rice <richmond@ricey.org>
2025-09-24 23:46:39 +0100

    try x64 again

--- Blaise.Nuget.Api.Core/Services/SqlService.cs
+++ Blaise.Nuget.Api.Core/Services/SqlService.cs
@@ -1,6 +1,8 @@
 namespace Blaise.Nuget.Api.Core.Services
 {
+    using System;
     using System.Collections.Generic;
+    using System.Data;
     using Blaise.Nuget.Api.Contracts.Enums;
     using Blaise.Nuget.Api.Contracts.Extensions;
     using Blaise.Nuget.Api.Contracts.Models;
@@ -11,24 +13,29 @@ namespace Blaise.Nuget.Api.Core.Services
     {
         public IEnumerable<string> GetCaseIds(string connectionString, string questionnaireName)
         {
+            ValidateInputs(connectionString, questionnaireName);
+
             var caseIds = new List<string>();
             var databaseTableName = GetDatabaseTableNameForm(questionnaireName);
+
             using (var con = new MySqlConnection(connectionString))
             using (var cmd = new MySqlCommand())
             {
                 con.Open();
                 cmd.Connection = con;
-                cmd.CommandText = $"SELECT {SqlFieldType.CaseId.FullName()} from {databaseTableName}";
+                cmd.CommandText = $"SELECT `{SqlFieldType.CaseId.FullName()}` FROM `{databaseTableName}`";
 
                 using (var reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        caseIds.Add(reader[0].ToString());
+                        var caseId = reader[0]?.ToString();
+                        if (!string.IsNullOrEmpty(caseId))
+                        {
+                            caseIds.Add(caseId);
+                        }
                     }
                 }
-
-                con.Close();
             }
 
             return caseIds;
@@ -36,6 +43,8 @@ namespace Blaise.Nuget.Api.Core.Services
 
         public IEnumerable<string> GetEditingCaseIds(string connectionString, string questionnaireName)
         {
+            ValidateInputs(connectionString, questionnaireName);
+
             var caseIds = new List<string>();
             var databaseTableName = GetDatabaseTableNameForm(questionnaireName);
             var databaseUneditedTableName = GetDatabaseTableNameUneditedForm(questionnaireName);
@@ -50,23 +59,25 @@ namespace Blaise.Nuget.Api.Core.Services
             {
                 con.Open();
                 cmd.Connection = con;
-                cmd.CommandText = $"SELECT QUESTIONNAIRE.{SqlFieldType.CaseId.FullName()} " +
-                                  $"FROM {databaseTableName} QUESTIONNAIRE " +
-                                  $"JOIN {databaseUneditedTableName} UNEDITED " +
-                                  $"ON QUESTIONNAIRE.{SqlFieldType.CaseId.FullName()} = UNEDITED.{SqlFieldType.CaseId.FullName()} " +
-                                  $"AND (QUESTIONNAIRE.{SqlFieldType.Edited.FullName()} = 1 " +
-                                  $"OR (QUESTIONNAIRE.{SqlFieldType.EditLastUpdated.FullName()} IS NULL AND UNEDITED.{SqlFieldType.EditLastUpdated.FullName()} IS NULL) " +
-                                  $"OR (QUESTIONNAIRE.{SqlFieldType.EditLastUpdated.FullName()} = UNEDITED.{SqlFieldType.EditLastUpdated.FullName()}))";
+                cmd.CommandText = $"SELECT QUESTIONNAIRE.`{SqlFieldType.CaseId.FullName()}` " +
+                                  $"FROM `{databaseTableName}` QUESTIONNAIRE " +
+                                  $"JOIN `{databaseUneditedTableName}` UNEDITED " +
+                                  $"ON QUESTIONNAIRE.`{SqlFieldType.CaseId.FullName()}` = UNEDITED.`{SqlFieldType.CaseId.FullName()}` " +
+                                  $"AND (QUESTIONNAIRE.`{SqlFieldType.Edited.FullName()}` = 1 " +
+                                  $"OR (QUESTIONNAIRE.`{SqlFieldType.EditLastUpdated.FullName()}` IS NULL AND UNEDITED.`{SqlFieldType.EditLastUpdated.FullName()}` IS NULL) " +
+                                  $"OR (QUESTIONNAIRE.`{SqlFieldType.EditLastUpdated.FullName()}` = UNEDITED.`{SqlFieldType.EditLastUpdated.FullName()}`))";
 
                 using (var reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        caseIds.Add(reader[0].ToString());
+                        var caseId = reader[0]?.ToString();
+                        if (!string.IsNullOrEmpty(caseId))
+                        {
+                            caseIds.Add(caseId);
+                        }
                     }
                 }
-
-                con.Close();
             }
 
             return caseIds;
@@ -74,24 +85,27 @@ namespace Blaise.Nuget.Api.Core.Services
 
         public IEnumerable<CaseIdentifierModel> GetCaseIdentifiers(string connectionString, string questionnaireName)
         {
+            ValidateInputs(connectionString, questionnaireName);
+
             var caseIdentifiers = new List<CaseIdentifierModel>();
             var databaseTableName = GetDatabaseTableNameForm(questionnaireName);
+
             using (var con = new MySqlConnection(connectionString))
             using (var cmd = new MySqlCommand())
             {
                 con.Open();
                 cmd.Connection = con;
-                cmd.CommandText = $"SELECT {SqlFieldType.CaseId.FullName()}, {SqlFieldType.PostCode.FullName()} from {databaseTableName}";
+                cmd.CommandText = $"SELECT `{SqlFieldType.CaseId.FullName()}`, `{SqlFieldType.PostCode.FullName()}` FROM `{databaseTableName}`";
 
                 using (var reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        caseIdentifiers.Add(new CaseIdentifierModel(reader[0].ToString(), reader[1].ToString()));
+                        var caseId = reader[0]?.ToString() ?? string.Empty;
+                        var postCode = reader[1]?.ToString() ?? string.Empty;
+                        caseIdentifiers.Add(new CaseIdentifierModel(caseId, postCode));
                     }
                 }
-
-                con.Close();
             }
 
             return caseIdentifiers;
@@ -99,30 +113,40 @@ namespace Blaise.Nuget.Api.Core.Services
 
         public string GetPostCode(string connectionString, string questionnaireName, string primaryKey)
         {
-            string postCode;
+            ValidateInputs(connectionString, questionnaireName);
+
+            if (string.IsNullOrWhiteSpace(primaryKey))
+            {
+                throw new ArgumentException("Primary key cannot be null or empty", nameof(primaryKey));
+            }
+
             var databaseTableName = GetDatabaseTableNameForm(questionnaireName);
+
             using (var con = new MySqlConnection(connectionString))
             using (var cmd = new MySqlCommand())
             {
                 con.Open();
                 cmd.Connection = con;
-                cmd.CommandText = $"SELECT {SqlFieldType.PostCode.FullName()} from {databaseTableName} WHERE {SqlFieldType.CaseId.FullName()} = {primaryKey}";
+                cmd.CommandText = $"SELECT `{SqlFieldType.PostCode.FullName()}` FROM `{databaseTableName}` WHERE `{SqlFieldType.CaseId.FullName()}` = @primaryKey";
+                cmd.Parameters.AddWithValue("@primaryKey", primaryKey);
 
                 using (var reader = cmd.ExecuteReader())
                 {
-                    reader.Read();
-                    postCode = reader[0].ToString();
+                    if (!reader.Read())
+                    {
+                        throw new InvalidOperationException($"No record found for primary key: {primaryKey}");
+                    }
+
+                    return reader[0]?.ToString() ?? string.Empty;
                 }
-
-                con.Close();
             }
-
-            return postCode;
         }
 
         public bool DropQuestionnaireTables(string connectionString, string questionnaireName)
         {
-            // implemented this way as StatNeth don't currently provide a way to drop the SQL tables via the API
+            ValidateInputs(connectionString, questionnaireName);
+
+            // Implemented this way as StatNeth don't currently provide a way to drop the SQL tables via the API
             var firstDatabaseTableName = GetDatabaseTableNameForm(questionnaireName);
             var secondDatabaseTableName = GetDatabaseTableNameDml(questionnaireName);
 
@@ -141,12 +165,29 @@ namespace Blaise.Nuget.Api.Core.Services
                     cmd.ExecuteNonQuery();
                 }
             }
-            catch
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+            catch (Exception ex)
             {
                 return false;
             }
 
             return true;
+        }
+
+        private static void ValidateInputs(string connectionString, string questionnaireName)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("Connection string cannot be null or empty", nameof(connectionString));
+            }
+
+            if (string.IsNullOrWhiteSpace(questionnaireName))
+            {
+                throw new ArgumentException("Questionnaire name cannot be null or empty", nameof(questionnaireName));
+            }
         }
 
         private static string GetDatabaseTableNameUneditedForm(string questionnaireName)
@@ -166,24 +207,24 @@ namespace Blaise.Nuget.Api.Core.Services
 
         private bool TableExists(string connectionString, string databaseTableName)
         {
-            bool tableExists;
             using (var con = new MySqlConnection(connectionString))
             using (var cmd = new MySqlCommand())
             {
                 con.Open();
                 cmd.Connection = con;
-                cmd.CommandText = $"SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{databaseTableName}'";
+                cmd.CommandText = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @tableName";
+                cmd.Parameters.AddWithValue("@tableName", databaseTableName);
 
                 using (var reader = cmd.ExecuteReader())
                 {
-                    reader.Read();
-                    tableExists = reader[0].ToString() == "1";
+                    if (reader.Read())
+                    {
+                        return Convert.ToInt32(reader[0]) > 0;
+                    }
                 }
-
-                con.Close();
             }
 
-            return tableExists;
+            return false;
         }
     }
 }
